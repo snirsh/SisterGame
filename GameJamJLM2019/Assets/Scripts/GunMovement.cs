3302917c047ae1a90af65a909245b317e5285702
Snir <s.sharristh@gmail.com>
2019-12-30 01:31:20 +0200

    there's a cross

--- GameJamJLM2019/Assets/Scripts/GunMovement.cs
+++ GameJamJLM2019/Assets/Scripts/GunMovement.cs
@@ -14,16 +14,10 @@ public class GunMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 currentPos = Input.mousePosition;
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
-        // inputVector = Vector2.ClampMagnitude(inputVector, 1);
-        isoRenderer.SetDirection(inputVector);
-        
-        // Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
-        // float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        // transform.rotation = Quaternion.Euler(0, 0, transform.eulerAngles.z);
-        // isoRenderer.SetDirection(dir);
+        Vector3 relativeLookAtPos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        var angle = Mathf.Atan2(relativeLookAtPos.y, relativeLookAtPos.x);
+        transform.rotation = Quaternion.Euler(0, 0, angle * Mathf.Rad2Deg);
+        Debug.DrawLine(transform.position, relativeLookAtPos, Color.red);
+        Debug.Log(relativeLookAtPos);
     }
 }
