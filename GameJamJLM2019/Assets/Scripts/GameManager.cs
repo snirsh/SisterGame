2c709652df392b9e5e3f307480d7a3abc696b055
Snir <s.sharristh@gmail.com>
2019-12-30 12:46:21 +0200

    ...

---
+++ GameJamJLM2019/Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject Nun;
+    
+    public static GameManager Instance { get; private set; }
+
+    protected void Awake()
+    {
+        Instance = this;
+    }
+}
